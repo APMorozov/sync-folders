7b95bf7dd2824fa1fc69953575d48b464ea89748
APMorozov <morozov-sasha200@yandex.ru>
2025-12-18 18:35:52 +0400

    Добавил доп валидацию по коду

--- src/core/EventBus.py
+++ src/core/EventBus.py
@@ -11,10 +11,10 @@ class EventBus(QObject):
 
     def __init__(self, pc_folder: str):
         super().__init__()
-        self.pc_folder_name = Path(pc_folder).name
+        self.pc_folder = Path(pc_folder)
 
     def update_pc_folder(self, pc_folder: str):
-        self.pc_folder_name = Path(pc_folder).name
+        self.pc_folder = Path(pc_folder)
 
     def usb_monitor(self):
         known = set()
@@ -28,17 +28,22 @@ class EventBus(QObject):
             new_drives = drives - known
 
             for drive in new_drives:
-                if self._is_valid_flash(drive):
-                    self.usb_detected.emit(Path(drive) / Path(self.pc_folder_name))
+                if self.is_valid_flash(drive):
+                    self.usb_detected.emit(Path(drive) / Path(self.pc_folder).name)
 
             known = drives
             time.sleep(1)
 
-    def _is_valid_flash(self, drive: str) -> bool:
+    def is_valid_flash(self, drive: str) -> bool:
         drive = Path(drive)
+        pc_folder_name = self.pc_folder.name
         print("Drive: ", drive)
-        return (
-            (drive / self.pc_folder_name).is_dir()
-            and (drive / self.pc_folder_name / ".sync").is_dir()
-            and (drive / self.pc_folder_name / ".sync" / "code").is_file()
-        )
+        valid = (((drive / pc_folder_name).is_dir()
+                 and (drive / pc_folder_name / ".sync").is_dir())
+                 and (drive / pc_folder_name / ".sync" / "code").is_file())
+        if valid:
+            flash_code = (drive / pc_folder_name / ".sync" / "code")
+            pc_code =(self.pc_folder / ".sync" / "code")
+            if flash_code.read_text("utf-8") == pc_code.read_text("utf-8"):
+                return True
+        return False
